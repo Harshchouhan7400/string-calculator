86306f4b71d4ccfe5095e280dbecb7c38a528252
HarshChouhan <harshchouhan7400@gmail.com>
2025-04-22 16:27:50 +0530

    added constructor and empty spec for sum method

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,2 +1,8 @@
 class StringCalculator
+  def initialize(string_numbers)
+    @string_numbers = string_numbers
+  end
+
+  def sum_of_numbers 
+  end
 end
--- string_calculator_spec.rb
+++ string_calculator_spec.rb
@@ -2,4 +2,6 @@ require 'rspec'
 require_relative 'string_calculator'
 
 RSpec.describe StringCalculator do
+  describe '#sum_of_numbers' do
+  end
 end
