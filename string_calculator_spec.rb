98e2521f7efb846dcf61a00413a3a8fbc06b56e7
HarshChouhan <harshchouhan7400@gmail.com>
2025-04-24 12:35:54 +0530

    Added new tests for different delimiters and fix old test cases

--- string_calculator_spec.rb
+++ string_calculator_spec.rb
@@ -17,5 +17,15 @@ RSpec.describe StringCalculator do
       string_calculator = StringCalculator.new("1,5")
       expect(string_calculator.sum_of_numbers).to eq(6)
     end
+
+    it "handles newlines and commas between numbers and returns their sum" do
+      string_calculator = StringCalculator.new("1\n2,3")
+      expect(string_calculator.sum_of_numbers).to eq(6)
+    end
+
+    it "handles mixed delimiters (comma and newline) and returns their sum" do
+        string_calculator = StringCalculator.new("1\n2,3\n4")
+        expect(string_calculator.sum_of_numbers).to eq(10)
+    end
   end
 end
